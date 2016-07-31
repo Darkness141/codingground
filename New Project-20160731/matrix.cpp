0e036d96de2853b2a446fce91931610a028526c8
Darkness141 <wongwaiho77@gmail.com>
2016-07-31 15:43:59 +0800

    Create matrix.cpp

---
+++ New Project-20160731/matrix.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int i = 4, j;
+   while (i>0){
+       j = 1;
+       while (j<=4){
+           if (i==j){
+               cout << "1";
+           }else{
+               cout << "0";
+           }
+           j = j + 1;
+       }
+        i = i - 1;
+        cout << endl;
+       
+   }
+   system("pause");
+  
+}
+
+

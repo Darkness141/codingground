2bcf5fb8a753cc534b3f2dd52343e45ddd220bd5
Darkness141 <wongwaiho77@gmail.com>
2016-07-31 17:18:31 +0800

    Create max.cpp

---
+++ New Project-20160731/max.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    int score[10];
+    int max = 0;
+    for (int i=0; i<10;i++){
+        score[i] = rand()%101;
+        cout << "The " << i+1 << " student's score is " << score[i] << endl;
+    }
+    for (int i=0; i<10; i++){
+        if (max < score[i]){
+            max = score[i];
+        }
+    }
+    cout << "The max score is " << max<<endl;
+    system("pause");
+   
+}
+

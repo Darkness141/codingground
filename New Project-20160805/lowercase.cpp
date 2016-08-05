c2caac62d83bb3ac4d89799c40539a19c7426a8c
Darkness141 <wongwaiho77@gmail.com>
2016-08-06 02:44:25 +0800

    Create lowercase.cpp

---
+++ New Project-20160805/lowercase.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    cout << "Enter a string";
+    cin >> s;
+    for (int i=0;i<s.length();i++){
+        s.at(i) = tolower(s.at(i));
+    }
+    cout << s;
+    system("pause");
+}
+

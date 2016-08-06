1527c1f472e2d0ef826f209e131df25f762341a6
Darkness141 <wongwaiho77@gmail.com>
2016-08-06 11:13:14 +0800

    Created by Coding Ground Client

---
+++ New Project-20160806/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    double n, r;
+    cout << "Enter the degree  ";
+    cin >> n;
+    r = n/180*3.1415926;
+    cout << "sin: " << sin(r) << endl;
+    cout << "cos: " << cos(r) << endl;
+    cout << "tan: " << tan(r) << endl;
+    system("pause");
+    
+    
+    
+   
+}
+
